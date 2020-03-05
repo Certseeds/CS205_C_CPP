5b45bab9ce3ad3ff8e52e16c760b695dc9fa198d
Certseeds <nanoseedskc@gmail.com>
2020-03-05 23:39:11 +0800

    finish lab 02, begin assignment01, support catch-unit-test.

--- lab02/lab02_01.cpp
+++ lab02/lab02_01.cpp
@@ -30,3 +30,23 @@ int main() {
     }
     return 0;
 }
+int main_lab02_01() {
+    cout << "Result:" << endl;
+    printf("%-2s%-s %-2s%s  %s%-5s%s\n", "x", "value",
+        "y", "value", "Expressions", " ", "Result");
+    //cout << "x value y value  Expressions   Result"<<endl;
+    const int x = 10;
+    const int y = 5;
+    vector<int> x = { 1,2,3,4,5 };
+    for (auto i : x) {
+        cout << i << endl;
+    }
+    char str1[5][4] = { "y+3","y-2","y*5","x/y","x%y" };
+    vector<int> str2 = { y + 3,y - 2,y * 5,x / y,x % y };
+    for (int i = 0; i < 5; i++) {
+        printf("%-3d|%4s%-2d|%6s", x, " ", y, " ");
+        printf("x=%-14s|x=%-4d", str1[i], str2[i]);
+        cout << endl;
+    }
+    return 0;
+}
