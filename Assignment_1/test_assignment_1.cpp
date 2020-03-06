32d32f5e58ea2535485f738404e2bf61f747b22f
Certseeds <nanoseedskc@gmail.com>
2020-03-06 21:46:34 +0800

    finish assignment_1. update .gitignore for ignore *.png.

--- Assignment_1/test_assignment_1.cpp
+++ Assignment_1/test_assignment_1.cpp
@@ -17,12 +17,47 @@ TEST_CASE("test_judge_legal", "[judge_legal]") {
 	REQUIRE(1 == judge_legal("Rio de Janeiro, Brazil"));
 	REQUIRE(0 == judge_legal("114514@gmail.comn"));
 	REQUIRE(0 == judge_legal("C#come from microsoft"));
-	REQUIRE(1 == judge_legal(""));
 
 }
+
+TEST_CASE("test_judge_legal_2", "[judge_legal]") {
+	REQUIRE(1 == judge_legal2("+0.114514 -0.1919810"));
+	REQUIRE(1 == judge_legal2("+1.114514 -0.1919810"));
+	REQUIRE(1 == judge_legal2("+.114514 -.1919810"));
+	REQUIRE(1 == judge_legal2("+1.14514 -1.919810"));
+	REQUIRE(1 == judge_legal2("+11.4514 -19.19810"));
+	REQUIRE(1 == judge_legal2("+114.514 -191.9810"));
+	REQUIRE(1 == judge_legal2("+1145.14 -1919.810"));
+	REQUIRE(1 == judge_legal2("+11451.4 -19198.10"));
+	REQUIRE(1 == judge_legal2("+114514. -1919810."));
+	REQUIRE(0 == judge_legal2("114514@gmail.comn"));
+	REQUIRE(0 == judge_legal2("C#come from microsoft"));
+}
+TEST_CASE("delete_space", "[test]") {
+	string str = "12345 678910";
+	REQUIRE(str == delete_space("   12345 678910   "));
+	REQUIRE(str == delete_space("12345 678910   "));
+	REQUIRE(str == delete_space("   12345 678910"));
+	REQUIRE(str == delete_space("12345 678910"));
+	REQUIRE("" == delete_space(""));
+}
+TEST_CASE("test_splits", "[test]") {
+	string str = "12345 678910";
+	REQUIRE("12345" == str.substr(0, str.find_first_of(' ')));
+	REQUIRE(" 678910" == str.substr(str.find_first_of(' '), str.size()));
+}
+TEST_CASE("test_check_double", "[test]") {
+	REQUIRE(1 == check_double("1123"));
+	REQUIRE(1 == check_double("-1.123"));
+	REQUIRE(1 == check_double("+1.123"));
+	REQUIRE(1 == check_double("+1.123"));
+	REQUIRE(1 == check_double("-0.1123"));
+	REQUIRE(1 == check_double("-.1123"));
+}
 TEST_CASE("test1", "[test]") {
-	cout << 1 << endl;
 }
 TEST_CASE("mains", "[run]") {
 	CHECK(0 == sub_main());
+	cin.get();
+	cin.get();
 }
