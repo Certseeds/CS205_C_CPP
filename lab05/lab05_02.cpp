0a0fa27866ad7c757cd888ddab6455c6d849b556
Killer_Quinn <51754303+Certseeds@users.noreply.github.com>
2020-03-19 16:43:15 +0800

    begin leb05 exercise.

---
+++ lab05/lab05_02.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int32_t sub_main_05_02();
+#ifndef _UNIT_TESTING_LAB_05_
+#define _UNIT_TESTING_LAB_05_
+int main() {
+    int32_t will_return = sub_main_05_02();
+    cin.get();
+    cin.get();
+    return will_return;
+}
+#endif // !_UNIT_TESTING_LAB_05_
+int32_t sub_main_05_02() {
+    return 0;
+}
---
+++ lab05/lab05_03.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int32_t sub_main_05_03();
+#ifndef _UNIT_TESTING_LAB_05_
+#define _UNIT_TESTING_LAB_05_
+int main() {
+    int32_t will_return = sub_main_05_03();
+    cin.get();
+    cin.get();
+    return will_return;
+}
+#endif // !_UNIT_TESTING_LAB_05_
+int32_t sub_main_05_03() {
+    return 0;
+}
